9e2fcf4b66816684bb3eae0b7ef5d3b94254d30b
Nuxhi <78807626+Nuxhi@users.noreply.github.com>
2025-11-19 21:44:55 +0100

    Add files via upload

---
+++ Exercice4/main.py
@@ -0,0 +1,49 @@
+from typing import Union
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+
+app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://localhost:3000"] pour limiter
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+compteur = 0
+
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
+
+
+@app.get("/compteurPlus")
+def lire_compteur_plus():
+    global compteur
+    compteur += 1
+    return {"compteur": compteur}
+
+
+@app.get("/compteurMoins")
+def lire_compteur_moins():
+    global compteur
+    compteur -= 1
+    return {"compteur": compteur}
+
+
+@app.get("/GetCompteur")
+def get_compteur():
+    global compteur
+    return {"compteur": compteur}
+
+
+
