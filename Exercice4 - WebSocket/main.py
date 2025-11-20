1b64d628a02b71cefc3a9af43829fd97dd53ef1c
Nuxhi <fabien8000@hotmail.com>
2025-11-20 12:06:34 +0100

    allow cross origini

--- Exercice4 - WebSocket/main.py
+++ Exercice4 - WebSocket/main.py
@@ -1,43 +1,106 @@
 from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+
 from fastapi.responses import HTMLResponse
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-html = """
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Chat</title>
-    </head>
-    <body>
-        <h1>WebSocket Chat</h1>
-        
-        <form action="" onsubmit="sendMessage(event)">
-            <input type="text" id="messageText" autocomplete="off"/>
-            <button>Send</button>
-        </form>
-        <ul id='messages'>
-        </ul>
-        <script>
-            var ws = new WebSocket("ws://localhost:8000/ws");
-            ws.onmessage = function(event) {
-                var messages = document.getElementById('messages')
-                var message = document.createElement('li')
-                var content = document.createTextNode(event.data)
-                message.appendChild(content)
-                messages.appendChild(message)
-            };
-            function sendMessage(event) {
-                var input = document.getElementById("messageText")
-                ws.send(input.value)
-                input.value = ''
-                event.preventDefault()
-            }
-        </script>
-    </body>
-</html>
-"""
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # ou ["http://localhost:3000"] pour limiter
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
+#L'IP DU SERVEUR DOIT ETRE http://127.0.0.1:8000
+compteur = 50 #on met la valeur du compteur a 0 a chauqe démarage du serveur
+
+
+###################
+# route du serveur 
+###################
+
+
+##route system
+@app.get("/")
+async def get():
+    return "/go to 127.0.0.1:3000, si vous n'etes pas membre du system vous n'avez rien a faire ici ♥"
+
+@app.get("/test")
+async def get_count():
+    return "+0+ = otot ɐ etet"
+
+
+##Route compteur
+@app.get("/GetCompteur")
+def get_compteur():
+    '''
+    Permet de mettre a jours la valeur du compteur
+    enlever le system du polling !!!! 
+    '''
+    global compteur
+    return {"compteur": compteur}
+
+
+@app.get("/compteurPlus")
+def lire_compteur_plus():
+    '''
+    incrémente le compteur de 1
+    '''
+    global compteur
+    compteur += 1
+    return {"compteur": compteur}
+
+
+@app.get("/compteurMoins")
+def compteur_moins():
+    '''
+    décrémente le compteur de 1
+    '''
+    global compteur
+    compteur -= 1
+    return {"compteur": compteur}
+
+
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    user = Client(websocket)                #on créer un objet user de la classe Client
+    await user.connect()                    #on demande a la classe d'établie une connexion
+
+
+
+    #/!\ attention, aucune gestion de suppresion de websocket mort présent dans le code
+    #alors on ignore les erreurs d'envoi afin d'éviter le crash du serveur
+    for ws in Manager.websocket_id_map.values():      #on notifie tous les clients connectés qu'un nouveau client est connecté
+        await ws.send_text(f"client {user.id} connecté")
+    
+
+    #Gestion de l'envoie de message
+    try:
+        while True:
+            data = await websocket.receive_text()
+            # Broadcast à tous
+            for ws in Manager.websocket_id_map.values():
+                 await ws.send_text(f"Client {user.id} : {data}")
+                    
+    except WebSocketDisconnect:
+        
+        message = await user.disconnect()
+        # Notifier tous les autres clients de la déconnexion
+        for ws in Manager.websocket_id_map.values():
+            await ws.send_text(message)
+
+        print(message)
+
+
+
+
+#################################################
+# Classe utilse a la gestion du chat, Id, Ws
+#################################################
 
 class Manager:
 
@@ -82,44 +145,3 @@ class Client:
         Manager.websocket_id_map.pop(self.id, None)
         return f"{self.id} déconnecté {Manager.websocket_id_map.values()}"
 
-
-
-
-
-@app.get("/")
-async def get():
-    return HTMLResponse(html)
-
-@app.get("/test")
-async def get_count():
-    return "+0+ = otot ɐ etet"
-
-@app.websocket("/ws")
-async def websocket_endpoint(websocket: WebSocket):
-    user = Client(websocket)                #on créer un objet user de la classe Client
-    await user.connect()                    #on demande a la classe d'établie une connexion
-
-
-
-    #/!\ attention, aucune gestion de suppresion de websocket mort présent dans le code
-    #alors on ignore les erreurs d'envoi afin d'éviter le crash du serveur
-    for ws in Manager.websocket_id_map.values():      #on notifie tous les clients connectés qu'un nouveau client est connecté
-        await ws.send_text(f"client {user.id} connecté")
-    
-
-    #Gestion de l'envoie de message
-    try:
-        while True:
-            data = await websocket.receive_text()
-            # Broadcast à tous
-            for ws in Manager.websocket_id_map.values():
-                 await ws.send_text(f"Client {user.id} : {data}")
-                    
-    except WebSocketDisconnect:
-        
-        message = await user.disconnect()
-        # Notifier tous les autres clients de la déconnexion
-        for ws in Manager.websocket_id_map.values():
-            await ws.send_text(message)
-
-        print(message)
